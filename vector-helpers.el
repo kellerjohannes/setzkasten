66e70898c7f0ebc98fb2a813d0d035a7590ff2dc
Johannes Keller <johannkell@gmail.com>
2022-01-17 10:17:20 +0100

    jk/vector helpers: changed cons to jk/vector constructor

--- vector-helpers.el
+++ vector-helpers.el
@@ -11,16 +11,16 @@
   (cdr v))
 
 (defun jk/add (a b)
-  (cons (+ (jk/x-coord a) (jk/x-coord b))
-	(+ (jk/y-coord a) (jk/y-coord b))))
+  (jk/vector (+ (jk/x-coord a) (jk/x-coord b))
+	     (+ (jk/y-coord a) (jk/y-coord b))))
 
 (defun jk/subtract (a b)
-  (cons (- (jk/x-coord a) (jk/x-coord b))
-	(- (jk/y-coord a) (jk/y-coord b))))
+  (jk/vector (- (jk/x-coord a) (jk/x-coord b))
+	     (- (jk/y-coord a) (jk/y-coord b))))
 
 (defun jk/scale (v f)
-  (cons (* (jk/x-coord v) f)
-	(* (jk/y-coord v) f)))
+  (jk/vector (* (jk/x-coord v) f)
+	     (* (jk/y-coord v) f)))
 
 (defun jk/distance (a b)
   (let ((vect (jk/subtract b a)))
@@ -30,15 +30,8 @@
 (defun jk/unit-vector (a b)
   (let ((vect (jk/subtract b a))
 	(len (jk/distance a b)))
-    (cons (/ (jk/x-coord vect) len)
-	  (/ (jk/y-coord vect) len))))
+    (jk/vector (/ (jk/x-coord vect) len)
+	       (/ (jk/y-coord vect) len))))
 
-(setq jk/a '(0 . 6))
-(setq jk/b '(-5 . 0))
-(setq jk/d '(5 . 0))
-
-(setq jk/t1 2)
-(setq jk/t2 1)
-
-(setq jk/e (jk/add jk/a (jk/add (jk/scale (jk/unit-vector jk/a jk/b) jk/t1)
-				(jk/scale (jk/unit-vector jk/a jk/d) jk/t2))))
+(defun jk/mirror-x (v x)
+  (jk/vector ... (jk/y-coord v)))
