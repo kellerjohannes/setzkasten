e425016533977def6de7509bef5d53e23d9f848d
Johannes Keller <johannes.keller@mab-bs.ch>
2022-04-10 11:04:36 +0200

    codebase householding, archiving elisp code

--- macro-test.lisp
+++
@@ -1,8 +0,0 @@
-(defmacro define-class (name &rest parameters)
-  `(defclass ,name
-     ,(mapcar #'(lambda (par) par) parameters)))
-
-
-
-(defclass dings ()
-  (asd (content1 :initarg :content1)))
