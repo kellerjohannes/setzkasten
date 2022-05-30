5d426dc47bc29e45c181c235fad03d1d7aa6c06a
Johannes Keller <johannes.keller@mab-bs.ch>
2022-05-30 08:28:56 +0200

    hygiene: removed elisp code, restructured lisp code and mei samples

--- elisp/generic-macros.el
+++
@@ -1,7 +0,0 @@
-(defmacro jk/let-if (if-condition then-bindings else-bindings &rest body)
-  `(if ,if-condition
-     (let ,then-bindings
-       ,@body)
-     (let ,else-bindings
-       ,@body)))
-
