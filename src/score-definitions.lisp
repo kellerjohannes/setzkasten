6c8c4d475bc3941748527173524310a2f5f27857
Johannes Keller <johannkell@gmail.com>
2022-11-09 11:25:24 +0100

    indentation cosmetics

--- src/score-definitions.lisp
+++ src/score-definitions.lisp
@@ -1140,5 +1140,18 @@
              b27 sb4 b38 b38 sb7 b27 b38 bl
              b38 sb7 b38 b38 sb4 b38 bl
              b38 sb4 b22 sb5 b22 sb6 b22 sb7 b38 bl
-             b38 sb7 b38 sb6 b38 sb5 b38 sb4 bl
-             )))))
+             b38 sb7 b38 sb6 b38 sb5 b38 sb4 bl)
+      (text 80 70
+            (58 nil "ſalto di ᴛrit.")
+            (369 nil "ſalto di ᴛri.")
+            (812 nil "ſalto di ᴛri.")
+            (1110 nil "ſalto di ᴛri.")
+            (1415 nil "gradi del Trit.")
+            (1801 nil "gradi del ᴛrit."))
+      (text 70 58
+            (57 nil "incompoſto")
+            (361 nil "incompoſto")
+            (809 nil "incompoſto")
+            (1107 nil "incompoſto")
+            (1405 nil "compoſto")
+            (1795 nil "compoſto"))))))
