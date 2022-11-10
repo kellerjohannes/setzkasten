9fb8d4f70fa471a4ba22f4b357b14199707f2d64
Johannes Keller <johannes.keller@mab-bs.ch>
2022-11-10 09:56:17 +0100

    libro1 done!

--- src/score-definitions.lisp
+++ src/score-definitions.lisp
@@ -1423,8 +1423,37 @@
             (64 nil "incomp.")
             (362 nil "incomp.")
             (682 nil "incomp.")
-            )))))
+            )))
+    ;; m1.51
+    (("l1c40m2" nil nil ,*score-bg-color*)
+     ((text 80 58
+            (580 1020 "ᴇſſempio de ɢradi,di Ɋuinte accidentali."))
+      (music 2226
+             cclef7 sh1 b22 sb1 b22 sh2 b22 sb2 b22 sh3 b22 sb3 b22 sh4 b22 sb4 b22 sh5 b22 sb5 bl
+             fl1 b22 sb1 b22 fl2 b22 sb2 b22 sbd2 b22 fl4 b22 sb4 b22 fl5 b22 sb5 bl
+             sh2 b22 sb2 b22 sh3 b22 sb3 b22 sh4 b22 sb4 b22 sh5 b22 sb5 b22 sh6 b22 sb6 bl)
+      (text 70 58
+            (60 684 "gradi della prima quinta acci.")
+            (798 620 "gradi della prima 5. acc.")
+            (1456 756 "gradi della quinta ſeconda acc."))
+      (text 70 58
+            (62 548 "per ſemitono min.comp.")
+            (798 622 "ꝑ ſemit.mag.aſcend.comp.")
+            (1476 640 "aſcend.per ſemit.mi.comp."))
+      (music 2226
+             cclef7 sh3 b22 sb3 b22 sh4 b22 sb4 b22 sh5 b22 sb5 b22 sh6 b22 sb6 b22 sh7 b22 sb7 bl
+             fl4 b22 sb4 b22 fl5 b22 sb5 b22 fl6 b22 sb6 b22 sb7 b22 fl8 b22 sb8 bl
+             sh4 b22 sb4 b22 sh5 b22 sb5 b22 sh6 b22 sb6 b22 sh7 b22 sb7 b22 sh8 b22 sb8 bl)
+      (text 70 58
+            (60 686 "gradi della terza quinta acci.")
+            (820 588 "gradi della terza 5.acci.")
+            (1476 702 "gradi della quarta quinta acci."))
+      (text 70 58
+            (62 666 "aſcend.per ſemit.min.comp.")
+            (822 572 "aſcen.per ſem.mag.cōp.")
+            (1474 634 "aſcend.per ſem.min.comp."))))))
 
+;; 978
 
 
 ;; Log book: image file number, new numbering, old numbering, origin for x-coordinates.
@@ -1435,7 +1464,8 @@
 ;; s048, l1c38m1 (ex 47), margin 1064
 ;; s049, l1c39m1 (ex 48), margin 393
 ;; s049, l1c39m2 (ex 49), margin 407
-;; s050, l1c40m1 (ex 50), margin 968, rotated -0.77
+;; s050, l1c40m1 (ex 50), margin 968, rotated -1.77
+;; s050, l1c40m2 (ex 51), margin 978, rotated -1.8
 
 
 
