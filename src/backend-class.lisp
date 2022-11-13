4496df38dbb5cf3fbf629aaeb258260c9cff3578
Johannes Keller <johannes.keller@mab-bs.ch>
2022-11-14 00:06:47 +0100

    external parser configuration not working

--- src/backend-class.lisp
+++ src/backend-class.lisp
@@ -17,14 +17,14 @@
 
 ;; to be deleted, this only concerns one single score, so it should be handled in the section with code about score processing, not on the level of the backend class (which looks after things that are relevant for all scores).
 
-(defclass backend-with-score (setzkasten-backend)
-  ((score-abstraction :initform (make-instance 'score) :accessor score-abstraction)))
+;; (defclass backend-with-score (setzkasten-backend)
+;;   ((score-abstraction :initform (make-instance 'score) :accessor score-abstraction)))
 
-(defmethod configure-score-abstraction ((backend backend-with-score) score-title score-filename)
-  (setf (title (score-abstraction backend)) title)
-  (setf (filename (score-abstraction backend)) filename))
+;; (defmethod configure-score-abstraction ((backend backend-with-score) score-title score-filename)
+;;   (setf (title (score-abstraction backend)) title)
+;;   (setf (filename (score-abstraction backend)) filename))
 
-;; TODO to be implemented
-(defmethod parse-score ((backend setzkasten-backend) score-data)
-  ;; add call to parse function, using score-abstraction
-  (declare (ignore backend score-data)))
+;; ;; TODO to be implemented
+;; (defmethod parse-score ((backend setzkasten-backend) score-data)
+;;   ;; add call to parse function, using score-abstraction
+;;   (declare (ignore backend score-data)))
