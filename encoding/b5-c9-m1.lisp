734bd7bdd32c5205b8a3a5bbe4dbca54a3f0f99f
Johannes Keller <johannes.keller@mab-bs.ch>
2022-12-13 09:48:52 +0100

    post adk

--- encoding/b5-c9-m1.lisp
+++ encoding/b5-c9-m1.lisp
@@ -1,9 +1,9 @@
 `((:header
    (:filename "b5-c9-m1")
-   (:alt-name "m5.005a")
+   (:alt-name "m5.005")
    (:comment "q021_s212, origin: line 1 987, line 2 979,")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title "Del secondo Alamire: ")
+   (:title "Del secondo Alamire")
    (:section-headings
     (s1 "Le quattro terze discendenti\\con le sue propinque")
     (s2 " \\Quinta")
@@ -14,9 +14,9 @@
     (s7 "Le quattro seste\\con le sue propinque")
     (s8 " \\Ottava"))
    (:section-captions
-    (s1 "Terza minore / Terza maggiore")
+    (s1 "Terza minore / terza maggiore")
     (s2 "")
-    (s3 "Sesta minore / Sesta maggiore")
+    (s3 "Sesta minore / sesta maggiore")
     (s4 "")
     (s5 "Terza minore / terza maggiore")
     (s6 "")
@@ -49,12 +49,6 @@
            (:section s3) b38 sh4 sb4 b22 sh4 b22 (:divider :regular) sb4 b38 bl
            b38 sh4 b22 sb4 b22 sh4 b22 sb4 b38 bl
            (:section s4) b38 sh4 b38 sb4 b38 b22 bl b38
-           (:section s5) (:newline) cclef7 b38 b38 sb6 b38 b38 (:divider :regular) sbd6 b38 b38 bl
-           b38 sh6 b38 sb6 b38 sb7 b38 bl
-           (:section s6) b38 b38 sh8 b38 sb8 b38 b38 b38 bl
-           (:section s7) b38 b38 sb9 b38 b38 (:divider :regular) sbd9 b38 b38 bl
-           b38 b38 sh9 b38 sb9 b38 sb10 b38 b22
-           (:section s8) cclef5 b38 b38 sh9 b38 sb9 b38 bl
            )
    (:text 70 58
           (304 nil "con le ſue propinque.")
@@ -67,6 +61,35 @@
            (:section s3) b38 b38 sh3 b38 sb3 b38 sbd3 b38 bl
            b38 sb3 b38 fl3 b38 sbd3 b22 b38 bl
            (:section s4) b38 b38 sh1 b38 sb1 b38 b38 bl
+           )
+   (:text 110 58
+          (96 nil "ᴛerza minor.")
+          (476 nil "ᴛerza magg.")
+          (1201 nil "Seﬆa minor.")
+          (1541 nil "ꜱeﬆa maggior."))
+   (:text 90 58
+          (172 1205 "Tutte le conſonanze diſcendenti con le propinque.")
+          (1540 609 "di Ala mi. re. ſecondo."))
+   (:text 110 58
+          (660 627 "ᴅel ſecondo ᴀla mi. re."))
+   (:text 90 58
+          (120 nil "Le quattro terze.aſcend.")
+          (829 nil "Ɋuinta.")
+          (1109 nil "Le quattro seſte.")
+          (1915 nil "ᴏttaua."))
+   (:music nil
+           (:voice v1)
+           (:section s5) (:newline) cclef7 b38 b38 sb6 b38 b38 (:divider :regular) sbd6 b38 b38 bl
+           b38 sh6 b38 sb6 b38 sb7 b38 bl
+           (:section s6) b38 b38 sh8 b38 sb8 b38 b38 b38 bl
+           (:section s7) b38 b38 sb9 b38 b38 (:divider :regular) sbd9 b38 b38 bl
+           b38 b38 sh9 b38 sb9 b38 sb10 b38 b22
+           (:section s8) cclef5 b38 b38 sh9 b38 sb9 b38 bl)
+   (:text 70 58
+          (288 nil "con le ſue propinque.")
+          (1431 nil "con le ſue propinque."))
+   (:music nil
+           (:voice v2)
            (:section s5) cclef7 b38 sh4 b22 sb4 b38 sh4 sb4 b38 bl
            b22 sh4 b22 sb4 b22 sh4 b22 sb4 b22 bl
            (:section s6) b38 b38 sh4 b38 sb4 b38 b38 bl
@@ -74,10 +97,11 @@
            b38 sh4 b22 sb4 b38 sh4 b22 sb4 b38 bl
            (:section s8) b22 b38 sh4 b38 sb4 b38 b38 bl)
    (:text 110 58
-          (96 nil "ᴛerza minor.")
-          (476 nil "ᴛerza magg.")
-          (1201 nil "Seﬆa minor.")
-          (1541 nil "ꜱeﬆa maggior."))
+          (49 nil "Terza minor.")
+          (459 nil "ᴛerza magg.")
+          (1145 nil "ꜱeſta minor.")
+          (1547 nil "ꜱeſta magg.")
+          (1934 nil "ᴏttaua."))
    (:text 70 58
-          (172 1205 "Tutte le conſonanze diſcendenti con le propinque.")
-          (1540 609 "di Ala mi. re. ſecondo."))))
+          (343 657 "Tutte le conſonanze aſcendenti.")
+          (1276 572 "di Ala mi. re. ſecondo."))))
