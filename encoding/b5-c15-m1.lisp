f4ac77f60c4fd9c792f5095e457f6b027e9dcddd
Johannes Keller <johannkell@gmail.com>
2022-12-16 15:33:29 +0100

    up to b5-c16 done, c17 started

---
+++ encoding/b5-c15-m1.lisp
@@ -0,0 +1,87 @@
+`((:header
+   (:filename "b5-c15-m1")
+   (:alt-name "m5.011")
+   (:comment "q021_s217, origin: line 1-2 518, line 3-4 225")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "[Del Gsolreut secondo]")
+   (:section-headings
+    (s1 "Le quattro terze")
+    (s2 "Quinta")
+    (s3 "Le quattro seste")
+    (s4 "Ottava")
+    (s5 "Le quattro terze")
+    (s6 "Quinta")
+    (s7 "Le quattro seste")
+    (s8 "Ottava"))
+   (:section-captions
+    (s1 "Terza minore / terza maggiore")
+    (s3 "Sesta minore / sesta maggiore")
+    (s5 "Terza minore / terza maggiore")
+    (s7 "Sesta minore / sesta maggiore"))
+   (:voice-labels
+    (:all v1 "")
+    (:all v2 ""))
+   (:line-headings
+    "Tutte le consonanze di Gsolreut secondo ascendenti"
+    "Tutte le consonanze di Gsolreut secondo discendenti"))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 100 58
+          (104 2024 "Tutte le conſonanze di ɢſolreut ſecondo aſcend. at diſcendenti, sono qui ſotto notate."))
+   (:text 100 58
+          (218 nil "le quattro terze")
+          (1011 nil "quinta")
+          (1319 nil "le quattro seſte")
+          (2037 nil "ottaua"))
+   (:music 2239
+           (:voice v1)
+           (:section s1) cclef7 b38 sh3 b38 b22 sb3 b38 b22 sh3 b38 b22 (:divider :regular) sb3 b38 bl
+           sh3 b38 sb3 b38 sh3 b38 sb3 bl
+           (:section s2) b38 sh3 b38 sb3 b38 bl
+           (:section s3) sh3 sb3 b38 sh3 b38 (:divider :regular) sb3 bl
+           b38 sh3 b38 sb3 b38 sh3 b38 sb3 b22 bl
+           (:section s4) b38 sh3 b38 sb3 bl)
+   (:text 90 58
+          (212 nil "3.min.")
+          (630 nil "3.mag.")
+          (1277 nil "6.min.")
+          (1632 nil "6.mag."))
+   (:music 2238
+           (:voice v2)
+           (:section s1) (:f-clef) max7 fclef7 b38 (:alt (:diplomatic sh6 b38 b22 sb6 b38 b22 fl6 b38 sbd6)
+                                                         (:idealised sh5 b38 b22 sb5 b38 b22 b38 b38 sbd5))
+           b38 bl
+           b38 sb5 b38 fl5 b38 sbd5 b38 b22 bl
+           (:section s2) b38 b38 sb3 b38 b22 b22 bl
+           (:section s3) b38 sh2 b38 sb2 b38 sbd2 b38 b22 bl
+           b38 b22 sb2 b38 b22 fl2 b38 b22 sbd2 b38 b22 bl
+           (:section s4) b22 sh0 b22 sb0 b22 b22 b22 b22 bl)
+   (:text 140 120
+          (215 nil "le quattro terze")
+          (914 nil (:alt (:diplomatic "quɪnta") (:idealised "quinta")))
+          (1282 nil "le quattro seste")
+          (1991 nil "ottaua"))
+   (:music 2218
+           (:voice v1)
+           (:section s5) (:newline) cclef3 b38 b38 sb1 b38 b22 b38 (:divider :regular) sbd1 b38 b38 bl
+           b38 sh1 b38 sb1 b38 fl2 b38 sb2 b38 bl
+           (:section s6) b38 sh3 b38 sb3 b38 bl
+           (:section s7) b38 b38 sb4 b38 b38 (:divider :regular) sbd4 b38 b38 bl
+           b38 sh4 b38 sb4 b38 fl5 b38 sb5 b38 bl
+           (:section s8) b38 b22 sh6 b38 sb6 b38 bl)
+   (:text 90 58
+          (211 nil "3.min.")
+          (527 nil "3.mag.")
+          (1223 nil "6.min.")
+          (1571 nil "6.mag."))
+   (:music 2226
+           (:voice v2)
+           (:section s5) (:f-clef) max7 fclef7 b22 sh7 b22 sb7 b22 sh7 b22 sb7 bl
+           b22 sh7 b38 sb7 b38 sh7 b38 sb7 b38 bl
+           (:section s6) b38 sh7 b38 sb7 b38 bl
+           (:section s7) b22 sh7 b22 sb7 b22 sh7 b22 sb7 b22 bl
+           b22 sh7 b38 sb7 b38 sh7 b22 sb7 b38 bl
+           (:section s8) b38 b22 sh7 b38 b22 sb7 b38 bl)))
---
+++ encoding/b5-c16-m1.lisp
@@ -0,0 +1,86 @@
+`((:header
+   (:filename "b5-c16-m1")
+   (:alt-name "m5.012")
+   (:comment "q021_s218, rotation: 1.11, origin: 977")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "[Del Gsolreut terzo]")
+   (:section-headings
+    (s1 "Le quattro terze\\discendenti")
+    (s2 " \\Quinta")
+    (s3 "Le quattro seste\\discendenti")
+    (s4 " \\Ottava")
+    (s5 "Le quattro terze\\ascendenti")
+    (s6 " \\Quinta")
+    (s7 "Le quattro seste\\ascendenti")
+    (s8 " \\Ottava"))
+   (:section-captions
+    (s1 "Terza minore / terza maggiore")
+    (s3 "Sesta minore / sesta maggiore")
+    (s5 "Terza minore / terza maggiore")
+    (s7 "Sesta minore / sesta maggiore"))
+   (:voice-labels
+    (:all v1 "")
+    (:all v2 ""))
+   (:line-headings
+    "Tutte le consonanze di Gsolreut terzo ascendenti"
+    "Tutte le consonanze di Gsolreut terzo discendenti"))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 70 58
+          (145 1950 "Tutte le conſonanze di ɢſolreut terzo aſcend. et diſcendenti,sono qui ſotto notate."))
+   (:text 90 58
+          (169 nil "le quattro terze diſcend.")
+          (900 nil "quinta")
+          (1262 nil "le quattro seſte")
+          (1993 nil "ottaua"))
+   (:music 2241
+           (:voice v1)
+           (:section s1) cclef7 b22 b22 fl4 sb4 b38 fl4 b38 (:divider :regular) sb4 b38 bl
+           b38 fl4 b38 sb4 b38 fl4 b38 sb4 b38 bl
+           (:section s2) b38 fl4 b38 sb4 b38 bl
+           (:section s3) b38 fl4 b38 sb4 b38 fl4 b38 (:divider :regular) sb4 b38 bl
+           b38 fl4 b38 sb4 b38 fl4 b38 sb4 b38 bl
+           (:section s4) b38 fl4 b38 sb4 b22 b22 bl)
+   (:text 70 58
+          (134 nil "3. min.")
+          (543 nil "3. mag.")
+          (1187 nil "6. min.")
+          (1594 nil "6. mag."))
+   (:music 2236
+           (:voice v2)
+           (:section s1) (:f-clef) max7 fclef7 b22 fl6 sb6 b38 (:alt (:diplomatic sh6 b22 sb6)
+                                                                     (:idealised sh5 b22 sb5))
+           b22 bl
+           b38 b38 sbd5 b38 b38 sb5 b38 b38 b38 bl
+           (:section s2) b38 fl4 b38 sb4 b38 bl
+           (:section s3) b22 b22 fl3 b38 sb3 b38 sh2 b38 sb2 b38 bl
+           b38 b38 sbd2 b38 b38 sb2 b38 b38 b38 bl
+           (:section s4) b38 fl1 b38 sb1 b38 b22 bl)
+   (:text 90 58
+          (166 nil "le quattro terze aſcend.")
+          (973 nil "quinta")
+          (1244 nil "le quattro seſte aſcendenti"))
+   (:music 2242
+           (:voice v1)
+           (:section s5) (:newline) cclef3 b38 b22 sbd1 b38 b22 sh1 b38 b22 (:divider :regular) sb1 b38 b22 bl
+           b38 fl2 b38 sb2 b38 fl2 b38 sbd2 b38 bl
+           (:section s6) b38 fl4 b38 sb4 b38 bl
+           (:section s7) b38 sbd4 b38 sh4 b38 b22 (:divider :regular) sb4 b38 bl
+           b38 fl5 b38 sb5 b38 fl5 b38 sbd5 b38 bl
+           (:section s8) b38 fl7 b38 sb7 b22 b22 bl)
+   (:text 110 78
+          (121 nil "3. min.")
+          (539 nil "3. mag.")
+          (1225 nil "6. min.")
+          (1628 nil "6. mag."))
+   (:music 2246
+           (:voice v2)
+           (:section s5) (:f-clef) max7 fclef7 b22 fl8 b38 sb8 b38 fl8 b38 sb8 b38 b22 bl
+           b38 fl8 b38 sb8 b38 fl8 b38 sb8 b22 b22 bl
+           (:section s6) b38 fl8 b38 sb8 b22 b22 bl
+           (:section s7) b38 fl8 b38 sb8 b38 fl8 b38 sb8 b38 bl
+           b38 fl8 b38 sb8 b38 fl8 b38 sb8 b38 bl
+           (:section s8) b38 fl8 b38 sb8 b38 b38 bl)))
