b587551653b332993723fc5cd6ee558536b675a9
Johannes Keller <johannkell@gmail.com>
2023-01-02 14:01:04 +0100

    working on soav'e dolce

--- encoding/b3-c51-m1.lisp
+++ encoding/b3-c51-m1.lisp
@@ -18,11 +18,33 @@
    (:height nil)
    (:background ,*score-bg-color*))
   (:data
-   (:music nil
+   (:music 2238
            (:voice v1)
            (:section s1) cclef1 fl7 (:key-signature nil nil nil nil nil nil :flat)
-           b22 met-perf-min-dim b22 br4 sbd3
+           b22 met-perf-min-dim b22 br4 sbd3 dot4 md3 sb3 sb3 brd4 sb5 sbrest5
+           sbd3 md3 b22 md3 m4 m4 sb2 sbd5 sbrest5
+           sbd3 sh4 sb4 md4 fl5 sb5 fl5 m5 sb5 sb2 mrest3
+           sbd2 b22 md2 fl3 m3 b22 m3 b22 md3 sbd6 fl6 sb6 sb6 bl b56
            )
-   (:text 70 58
-          (206 nil "Soau' e dolc' ardore")
-          )))
+   (:text 90 58
+          (213 439 "Soau' e dolc' ardore")
+          (684 nil "ÿ")
+          (1080 502 "che fra pianʼ e ſoſpiri")
+          (1625 520 "che fra piantʼ e ſoſpiri")
+          )
+   (:music 2225
+           (:voice v2)
+           (:section s1) cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           met-perf-min-dim b22 brrest4
+           b22 brd5 sh5 sb5 dot6 sh5 m5 sbd6 b22 sbd6 m4 dot4 sm3 b22 sb2 b22 brd2 sbrest9
+           b22 sb8 md7 dot8 smd7 m7 m7 sbd3 sb3 mrest5
+           sbd6 b22 md6 sb4 b22 m4 b22 m1 fl2 sb2 sbd2 b22 sb2 mrest3
+           sbd0 b22 md0 b22 m0 m7 bl
+           )
+   (:text 90 58
+          (230 nil "Soauʼ")
+          (494 213 "e dolcʼar")
+          (803 nil "dore")
+          (1009 nil "ÿ")
+          (1405 516 "che fra piantʼ e ſoſpiri")
+          (1966 nil "ÿ"))))
