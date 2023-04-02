90cb74229eb2ebf9ceae956da9c389613a39073a
Johannes Keller <johannkell@gmail.com>
2023-04-02 14:38:45 +0200

    working on musica prisca

---
+++ encoding/b3-c54-m1.lisp
@@ -0,0 +1,173 @@
+`((:header
+   (:filename "b3-c54-m1")
+   (:alt-name "m3.117")
+   (:comment "q021_s138, origin: 950 et al.; q021_s139, rotation: 1.87, origin: 461 et al.; q021_s140, rotation: ?, origin: ?")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "[Musica prisca caput]")
+   (:voice-labels
+    (:all v1 "[Soprano]")
+    (:all v2 "[Alto]")
+    (:all v3 "[Tenore]")
+    (:all v4 "[Basso]")))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:preamble-lilypond
+   (:lyrics
+    (s1 v1 "")
+    (s1 v2 "")
+    (s1 v3 "")
+    (s1 v4 "")))
+  (:data
+   ;; soprano
+   (:music 2221
+           (:voice v1)
+           (:section s1)
+           cclef1 b22 fl7 (:key-signature nil nil nil nil nil nil :flat)
+           b22 met-perf-min-dim b38 sb4 dot4 m3 b22 m5 b22 sb6 b22 m7 b22 sb8 b22 sb8 b22 mrest6 b22 m6 dot6
+           sb6 b22 m5 b22 m4 b22 m3 b22 sb2 b22 sb2 b22 mrest3 b22 sb5 b22 m5 b22 m5 b22 sb6 b22
+           m7 b22 m8 dot8 sb8 b22 m8 b22 sb8 b22 m7 b22 sb8 custos6)
+   (:text 90 58
+          (268 481 "Muſica priſca caput")
+          (832 nil "ij")
+          (1408 596 "tenebris modo ſuſtulit al")
+          (2041 nil "tis"))
+   (:music 2216
+           cclef1 fl7 (:key-signature nil nil nil nil nil nil :flat)
+           b22 mrest5 b22 m6 dot6 sm6 b22 m6 b22 m5 b22 m4 b22 sb3 b22 m4 b22 m4 dot4 sm4 b22 m4 b22 m4 b22 m3 b22 m4 dot4 sb4 b22
+           m4 b22 m4 dot4 cr3 b22 cr2 b22 m3 b22 br4 b22 mrest5 b22 sb5 b22 m5 b22 m5 b22 m3 b22 m4 dot4 sb4 custos6)
+   (:text 90 58
+          (191 1089 "Muſica priſca caput tenebris modo ſuſtulit")
+          (1351 nil "al")
+          (1597 nil "tis")
+          (1720 451 "ᴅulcibus ut nume⸗"))
+   (:music 2213
+           cclef1 fl7 (:key-signature nil nil nil nil nil nil :flat)
+           b22 m4 sh4 sb4 sh4 m4 b22 m5 dot6 sm6 b22 m7 nat7 m7 b22 m8 dot8 sm8 b22 m6 b22 m6 dot6 sm6 b22
+           m6 b22 m9 dot10 sm9 nat7 sb7 b22 sb7 b22 sb6 b22 mrest7 b22 m8 dot8 sm8 b22 m8 b22 m8 b22
+           m6 dot6 sm4 b22 m3 b22 custos2)
+   (:text 90 58
+          (128 274 "ris dulcibus")
+          (634 678 "ut numberis priſcis certantia")
+          (1392 nil "factis")
+          (1627 491 "dulcibus ut numeris"))
+   (:music 2209
+           cclef1 fl7 (:key-signature nil nil nil nil nil nil :flat)
+           b22 mrest3 b22 m2 dot2 sm2 b22 m2 sh2 m2 dot2 sh2 sm2 b22 m2 b22 sb4 b22 m3 b22 sb4 b22
+           mrest5 b22 sb5 b22 m3 b22 sb4 b22 sb3 b22 md3 b22 sbd3 b22 md3 b22 sb4 b22 mrest5 b22
+           sb4 b22 m4 b22 sbd7 b22 sbd7 nat7 sb7 nat7 sbd7 dot8 custos7)
+   (:text 90 58
+          (163 400 "pricis certantia")
+          (657 nil "fac")
+          (817 nil "tis")
+          (984 nil "facta")
+          (1142 nil "tua")
+          (1274 nil "ʜyp")
+          (1412 nil "polite")
+          (1634 nil "facta")
+          (1799 nil "tua")
+          (1961 nil "ʜyppo⸗"))
+   (:music 2210
+           cclef1 fl7 (:key-signature nil nil nil nil nil nil :flat)
+           b38 nat7 md7 b22 sb8 b22 sbd5 b22 sbd5 dot6 md5d b22 sb6 b22 md6 b22 md9 b22 smd9 b22 md9
+           sh8 sb8 b22 sbd9 b22 mrest7 b22 sb8 b22 m8 b22 brd6 b38 sbd7 dot8 md7 nat7 m7 dot8 nat7 sm7
+           nat7 sbd7 nat7 sbd7 nat7 sbd7 nat7 sbd7 b38 custos8)
+   (:text 90 58
+          (206 nil "lite")
+          (324 547 "excelſum ſuper aethera")
+          (912 nil "mittat")
+          (1134 210 "facta tua")
+          (1147 nil "ʜyppolite")
+          (1879 nil "excelſum"))
+   (:music 2201
+           cclef1 fl7 (:key-signature nil nil nil nil nil nil :flat)
+           b38 sb8 b38 m8 sh8 sb8 sh8 md8 b22 fl9 sb9 b22 fl9 b22 sb9 b38 longa9 b38 blbl
+           b118 b118 b118 b118 b118 b118 b118 b118 b118 b118 b118)
+   (:text 90 58
+          (133 nil "ſuper")
+          (310 nil "aethera")
+          (530 nil "mit")
+          (693 nil "tat."))
+
+   ;; second page, alto
+   (:music 2218
+           (:voice v2)
+           cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           b38 met-perf-min-dim b38 brrest4 b22 sb5 dot6 m6 b22 m7 b22 sb8 b22 m8 b22 sm7 b22 sm6 b22
+           m5 b22 sb6 b22 sbrest5 b22 mrest3 b38 sb4 b22 m4 b22 m3 b22 sb7 b22 m6 b22 m6 dot6 sm5 b22 m5 b22 sb8 b22
+           m8 b22 m9 dot10 sm9 b22 m9 b22 sb9 b22 m9 b38 custos8)
+   (:text 90 58
+          (293 nil "Muſica")
+          (504 nil "priſca ca")
+          (840 nil "put")
+          (1069 nil "tenebris")
+          (1302 nil "modo")
+          (1465 nil "ſuſtulit altis")
+          (1788 nil "ſuſtulit")
+          (2025 nil "altis"))
+   (:music 2219
+           cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           b22 mrest7 m8 dot8 sm8 b22 m8 b22 m7 b22 m6 b22 sb2 b22 m6 b22 m6 dot6 sm6 b22 m6 b22
+           md5 b22 m5d b22 m5d dot6 sm5d b22 m5b b22 m5d b22 m5d b22 m5d dot6 sm5d b22 m5d b22 m5d b22
+           m6 b22 m6 dot6 sm6 fl7 m7 b22 m7 b22 m7 custos9)
+   (:text 90 58
+          (170 696 "Muſica priſca caput tenebris")
+          (914 nil "mo")
+          (1020 nil "do")
+          (1110 nil "ſuſtulit")
+          (1327 nil "altis")
+          (1475 nil "ſustulit")
+          (1691 485 "altis dulcibus ut nu⸗"))
+   (:music 2220
+           ;; Schlüssel in der Quelle auf dem Kopf stehend
+           cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           b22 m9 b22 sb5 b22 mrest5 b22 sb6 m3 sb6 b22 m9 b22 sb9 b22 m9 b22 m5 b22 m5 dot6 sm5 b22 m3 b22 m3 dot4
+           sm3 sb6 b22 mrest7 sb9 sh8 m8 b22 mrest7 b22 fl7 b22 m7 dot8 fl7 b22 sm7 b22
+           fl7 b22 m7 b22 fl7 b22 m7 b22 m5d b22 custos5)
+   (:text 90 58
+          (140 nil "meris")
+          (360 676 "dulcibus ut numeris priſcis")
+          (1080 nil "certantia")
+          (1422 nil "factis")
+          (1697 nil "dulcibus")
+          (1986 nil "ut nu⸗"))
+   (:music 2220
+           cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           b38 sh5 m5 sh5 b22 mrest5 b22 m4 dot4 sm4 b22 m4 b22 sh1 m1 dot2
+           sh1 sm1 sh1 m1 b22 mrest3 b22 m5 b22 m2 b22 sb5 b22 m6 b22 sb7 b22 m9 b22 sbrest5 b22 sb2 b22 sbd5 dot6 md5 b22
+           sb3 b22 mrest5 b22 sb6 b22 m6 b22 sbd6 b22 sbd6 custos6)
+   (:text 90 58
+          (176 nil "meris")
+          (411 237 "priſcis cer")
+          (699 nil "tan")
+          (931 nil "tia")
+          (1080 379 "factis facta tua")
+          (1538 nil "ʜyppolite")
+          (1917 nil "facta tua"))
+   (:music 2231
+           cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           b22 sb6 b22 sbd9 dot10 md9 b22 sbd7 b22 sb7 b38 brd7 b22 m8 b38 m5d b38 m8 b22 fl9 b22 m9 dot10
+           fl9 b38 sm9 b38 md8 b22 sbd7 sh8 sb8 b38 mrestb22 sb8 b38 m8 b22
+           sbd5 fl9 b22 sb9 b22 mrest7 b22 m8 b38 m8 b22 md9 b38 sb6 b38 custos9)
+   (:text 90 58
+          (90 nil "ʜyppolite")
+          (374 429 "excelſum ſuper ae")
+          (884 nil "the")
+          (1177 nil "ra mittat")
+          (1500 nil "facta tua")
+          (1883 nil "ʜyppolite"))
+   (:music 2235
+           cclef5 fl4 (:key-signature nil nil nil nil nil nil :flat)
+           b38 sbd9 b38 sbd9 b38 sbd9 b38 sb7 b38 md7 b38 m7 dot8 sm9 b38 m9 b38 br9 sh8 longa8 b38 blbl
+           b22 b22 b22 b118 b118 b118 b118 b118 b118 b118 b118 b118)
+   (:text 90 58
+          (132 nil "ex")
+          (232 307 "celſum ſuper")
+          (584 nil "aethe")
+          (763 nil "ra")
+          (842 nil "mittat ."))
+   ;; tenore
+   (:music 2220
+           )
+   ))
