e3136c93bd4311c38397c8a6c971fc183de7fddc
Johannes Keller <johannkell@gmail.com>
2023-04-09 16:41:01 +0200

    working on b3-c32-m2 (done)

---
+++ encoding/b3-c32-m1.lisp
@@ -0,0 +1,60 @@
+`((:header
+   (:filename "b3-c32-m1")
+   (:alt-name "m3.063")
+   (:comment "q021_s109, rotation: 1.61, origin: 421")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "Esempio di molte cadentie del quarto modo, a quattro voci,\\della musica participata e mista per ♮ quadro")
+   (:voice-labels
+    (s1 v1 "Cadentie del soprano")
+    (s1 v2 "Cadentie del contr'alto")
+    (s1 v3 "Cadentie del tenore")
+    (s1 v4 "Cadentie del basso")))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 70 58
+          (157 1898 "Eſsempio di molte cadentie del Quarto modo,à quattro uoci , della muſica parti⸗"))
+   (:text 130 58
+          (725 752 "cipata & miſta per ♮. quadro."))
+   (:text 90 58
+          (680 541 "Cadentie del ꜱoprano."))
+   (:music 2232
+           (:voice v1)
+           (:section s1) cclef1 b38 mrest1 b22 sb2 sh1 m1 b22 sb2 b22 bl
+           (:section s2) b22 mrest3 b22 sb3 b22 m2 b22 sb3 b22 bl
+           (:section s3) b22 mrest5 sb4 b22 m3 b22 sb4 b22 bl
+           (:section s4) (:newline) b22 mrest5 sb5 sh4 m4 b22 sb5 b22 bl
+           (:section s5) b38 m7 b22 sb8 b38 m7 b22 sb8 b38 bl
+           (:section s6) b22 mrest7 b22 sb9 b22 sh8 m8 b38 sb9 b22 bl)
+   (:text 110 58
+          (672 597 "Cadentie del ᴄontrʼᴀlto."))
+   (:music 2230
+           (:voice v2)
+           (:section s1) cclef5 b22 b38 sb3 b38 sb3 b38 sb3 b38 bl
+           (:section s2) b22 sb5 b38 sb3 b38 sb3 b22 bl
+           (:section s3) b22 sb5 b38 sb5 b38 sb5 b22 bl
+           (:section s4) b38 sb6 b38 sb6 b38 sb4 b38 bl
+           (:section s5) b22 b22 m9 b22 m9 b22 sb9 b22 sb7 b22 b22 bl
+           (:section s6) b38 sb10 b22 b38 sb10 b22 sh8 sb8 b38 b22 bl)
+   (:text 110 58
+          (673 541 "Cadentie del Tenore."))
+   (:music 2236
+           (:voice v3)
+           (:section s1) cclef7 b38 b22 sb3 b38 sb2 b38 sb1 b38 b22 bl
+           (:section s2) m5 dot6 sm4 sb3 b22 sb2 b22 bl
+           (:section s3) b38 sb5 b22 sb4 b22 sb3 b38 bl
+           (:section s4) b38 sb6 b38 sb5 b38 sb4 b38 bl
+           (:section s5) b38 m8 b38 m9 b38 sb8 b38 sb7 b22 bl
+           (:section s6) b22 b38 sb10 b38 sb9 b22 b22 sb8 b22 b38 bl)
+   (:text 110 58
+          (665 477 "Cadentie del ʙaſſo ."))
+   (:music 2233
+           (:voice v4)
+           (:section s1) (:f-clef) max7 fclef7 b38 sb5 b38 sb2 b38 sb5 b38 bl
+           (:section s2) b38 sb2 b38 sb5 b38 sb2 b38 bl
+           (:section s3) b22 sb7 b38 sb4 b38 sb7 b22 bl
+           (:section s4) b38 sb1 b38 sb5 b38 sb1 b38 bl
+           (:section s5) b38 m8 b38 m4 b38 sb8 b38 sb4 b22 bl
+           (:section s6) b38 b22 sb5 b38 sb9 b38 sb5 b38 b22 bl)))
---
+++ encoding/b3-c32-m2.lisp
@@ -0,0 +1,65 @@
+`((:header
+   (:filename "b3-c32-m2")
+   (:alt-name "m3.064")
+   (:comment "q021_s110, origin: 955")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "Esempio di molte cadentie del secondo modo, a quattro voci,\\della musica participata e mista per ♮ quadro")
+   (:voice-labels
+    (s1 v1 "Cadentie del soprano")
+    (s1 v2 "Cadentie del contr'alto")
+    (s1 v3 "Cadentie del tenore")
+    (s1 v4 "Cadentie del basso")))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 70 58
+          (263 1768 "Eſſempio di molte cadentie del ꜱecondo Modo,à quattro uoci della Muſica"))
+   (:text 130 58
+          (699 922 "participata , & miſta , per ♮. quadro ."))
+   (:text 90 58
+          (654 547 "Cadentie del ꜱoprano ."))
+   (:music 2238
+           (:voice v1)
+           (:section s1) cclef3 b22 mrest1 sb1 (:alt (:diplomatic sh1) (:idealised sh0)) m0 sb1 bl
+           (:section s2) b22 mrest3 b22 sb3 m2 sb3 b22 bl
+           (:section s3) b22 mrest3 b22 sb4 sh3 m3 sb4 b22 bl
+           (:section s4) (:newline) mrest5 sb6 m5d sb6 b22 bl
+           (:section s5) mrest5 sb5 m4 m5 sb7 sh6 m6 sb7 b22 bl
+           (:section s6) mrest7 sb8 sh7 m7 sb8 bl
+           (:section s7) (:alt (:diplomatic mrest7) (:idealised mrest3)) sb4 sh3 m3 br4 b22 blbl)
+   (:text 110 58
+          (671 630 "Cadentie del ᴄontrʼAlto."))
+   (:music 2250
+           (:voice v2)
+           (:section s1) cclef7 b38 sb2 b22 sb2 b22 sb2 b22 bl
+           (:section s2) b22 mrest3 m4 b22 sb4 b22 sb2 bl
+           (:section s3) b22 mrest5 m5 b22 sb5 sh3 sb3 b22 bl
+           (:section s4) mrest7 m7 b22 sb7 sb7 bl
+           (:section s5) b22 m6 b22 sb7 m5 sb6 b22 sb8 b22 sb8 bl
+           (:section s6) mrest7 m7 b22 sb6 b22 sb5 bl
+           ;; mögliche Idealisierung hier
+           (:section s7) b22 fl6 mrest7 m6 sb5 b22 br5 b22 blbl)
+   (:text 110 58
+          (652 536 "Cadentie del Tenore."))
+   (:music 2245
+           (:voice v3)
+           (:section s1) (:f-clef) max7 fclef7 b22 sb4 sb3 sb2 b22 bl
+           (:section s2) b22 sb6 b22 sb5 b22 sb4 bl
+           (:section s3) b22 b22 sb7 b38 sb6 b38 sb5 b38 bl
+           (:section s4) sb9 b22 sb8 b22 sb7 b22 bl
+           (:section s5) b22 sb8 m7 b22 m9 b22 sb8 b22 sb9 sb10 bl
+           (:section s6) b22 mrest5 m6 b22 sb6 sb6 b22 bl
+           (:section s7) b22 m8 dot8 sm7 sb6 br5 blbl)
+   (:text 110 58
+          (653 498 "Cadentie del ʙaſſo ."))
+   (:music 2248
+           (:voice v4)
+           (:section s1) (:f-clef) max9 fclef9 b22 sb4 sb1 sb4 b22 bl
+           (:section s2) mrest5 m6 b22 sb3 b22 sb10 bl
+           (:section s3) b38 mrest3 b22 m0 b22 sb4 b22 sb0 b38 bl
+           (:section s4) b22 sb2 b22 sb6 b22 sb2 b22 bl
+           (:section s5) b38 sb8 b22 sb9 b22 sb8 b22 sb7 b22 sb3 bl
+           (:section s6) mrest3 b22 m4 b22 sb1 b22 sb4 bl
+           (:section s7) b22 mrest5 m3 b22 sb4 b22 br0 b22 blbl)))
