a713ffc893b184855b498d04754b2035670ef124
Johannes Keller <johannkell@gmail.com>
2023-05-13 15:48:51 +0200

    added b3-c43-m1, was forgotten before

---
+++ encoding/b3-c43-m1.lisp
@@ -0,0 +1,56 @@
+`((:header
+   (:filename "b3-c43-m1")
+   (:alt-name "m3.091")
+   (:comment "q021_s122, rotation: 0, origin: 962")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:voice-labels
+    (s1 v1 "Cadentie cromatiche")
+    (s1 v2 "Cadentie cromatiche")
+    (s1 v3 "Cadentie cromatiche")
+    (s1 v4 "Cadentie cromatiche")
+    ))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 90 58
+     (799 nil "ᴄadentie ᴄromatiche."))
+   (:music 2216
+           (:voice v1)
+           (:section s1) cclef1 mrest5 m5 dot6 sh4 sm4 sm4 sh4 sm4 sb5 bl
+           (:section s2) b38 b38 sb3 b38 sb5 b38 sb3 bl
+           (:section s3) fl7 b38 sb7 b22 m6 m9 sb7 bl
+           (:section s4) sb5 mrest5 m6 fl7 m7 m5d bl
+           (:section s5) b38 mrest5 sb4 m3 sb4 bl
+           (:section s6) mrest5 sb6 sh5 m5 m5 sh5 m5 br6 bl)
+   (:text 90 58
+     (804 nil "ᴄadentie ᴄromatiche."))
+   (:music 2238
+           (:voice v2)
+           (:section s1) cclef5 b38 sb6 b38 sb6 b38 sb4 b38 bl
+           (:section s2) b38 b38 sb2 b38 b38 sb6 b38 b38 sb2 b22 b38 bl
+           (:section s3) b22 mrest7 b22 sb9 b22 sh8 b22 m8 b22 sb9 b22 bl
+           (:section s4) sb6 b22 sb3 b22 m6 b22 m4 b38 bl
+           (:section s5) b38 sb5 b38 sb5 b22 sb3 bl
+           (:section s6) br7 b22 sb7 sh5 (:alt (:diplomatic mrest3) (:idealised b22)) br5 b22 bl)
+   (:text 90 58
+     (811 nil "ᴄadentie ᴄromatiche."))
+   (:music 2241
+           (:voice v3)
+           (:section s1) cclef7 b22 fl6 b22 sb6 b22 sb5 b22 sb8 b38 bl
+           (:section s2) mrest7 m7 dot8 sm6 fl6 sm6 sm6 sb7 b22 bl
+           (:section s3) b22 sb8 b22 b22 m8 b38 m5d b38 sb8 b38 bl
+           (:section s4) mrest3 b22 sb4 sh3 b22 m3 b22 sb4 bl
+           (:section s5) fl5 b22 sb5 b22 sb4 sb7 bl
+           (:section s6) b38 sb7 b38 br6 b22 br9 b22 bl)
+   (:text 90 58
+     (807 nil "ᴄadentie ᴄromatiche."))
+   (:music 2242
+           (:voice v4)
+           (:section s1) (:f-clef) max7 fclef7 b38 sb1 b22 sb5 b22 sb1 b22 bl
+           (:section s2) b38 sb4 b38 b38 sb1 b38 b38 sb4 b38 b22 bl
+           (:section s3) b38 sb1 b38 b38 sb5 b22 b38 sb1 b22 bl
+           (:section s4) b38 sb3 b38 sb5 b38 sb1 b22 b22 bl
+           (:section s5) sb0 b38 sb4 b38 sb0 b22 bl
+           (:section s6) b38 sb2 b38 br6 b38 br2 b22 bl)))
