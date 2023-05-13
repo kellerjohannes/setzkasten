67d7100371ca7138bfd613710d5639ca08103f20
Johannes Keller <johannkell@gmail.com>
2023-05-13 13:55:31 +0200

    saved wrongly named b5-c59-m2, everything ok now

---
+++ encoding/b5-c59-m3.lisp
@@ -0,0 +1,130 @@
+`((:header
+   (:filename "b5-c59-m2")
+   (:alt-name "m5.089")
+   (:comment "q021_s271, rotation: 0.82, origin: 523")
+   (:creator "Vicentino21 / Johannes Keller")
+   ;; Titel zu diskutieren, für die ganze Serie von Beispielen
+   (:title "[?]")
+   (:section-headings
+    ;; Überschriften zu vereinheitlichen?
+    (s1 "Primo ordine diatonico")
+    (s2 "Secondo ordine diatonico")
+    (s3 "Terzo ordine diatonico")
+    (s4 "Quarto ordine diatonico")
+    (s5 "Del quinto ordine diatonico naturale")
+    (s6 "Del sesto ordine naturale diatonico")
+    (s7 "Del settimo ordine naturale diatonico")
+
+    (s8 "Primo ordine cromatico")
+    (s9 "Del secondo ordine cromatico")
+    (s10 "Del terzo ordine cromatico ed enarmonico")
+    (s11 "Del quarto ordine cromatico")
+    (s12 "Quinto ordine cromatico ed enarmonico")
+    (s13 "Sesto ordine cromatico ed enarmonico")
+    (s14 "Settimo ordine cromatico enarmonico")
+
+    (s15 "Del primo ordine cromatico ed enarmonico")
+    (s16 "Del secondo ordine cromatico")
+    (s17 "Del terzo ordine cromatico ed enarmonico")
+    (s18 "Del quarto ordine cromatico")
+    (s19 "Del quinto ordine cromatico ed enarmonico")
+    (s20 "Sesto ordine cromatico ed enarmonico")
+    (s21 "Settimo ordine cromatico enarmonico")
+
+    (s22 "Del primo ordine cromatico")
+    (s23 "Del secondo ordine cromatico")
+    (s24 "Del terzo ordine cromatico")
+    (s25 "Del quarto ordine cromatico")
+    (s26 "Del quinto ordine cromatico ed enarmonico")
+    (s27 "Sesto ordine cromatico ed enarmonico")
+    (s28 "Settimo ordine cromatico enarmonico")
+
+    (s29 "Del primo ordine cromatico ed enarmonico")
+    (s30 "Del secondo ordine cromatico")
+    (s31 "Del terzo ordine cromatico ed enarmonico")
+    (s32 "Del quarto ordine cromatico")
+    (s33 "Del quinto ordine cromatico ed enarmonico")
+    (s34 "Sesto ordine cromatico ed enarmonico")
+    (s35 "Settimo ordine cromatico enarmonico")
+
+    (s36 "Del primo ordine cromatico")
+    (s37 "Del secondo ordine cromatico")
+    (s38 "Del terzo ordine cromatico")
+    (s39 "Del quarto ordine cromatico")
+    (s40 "Del quinto ordine cromatico ed enarmonico")
+    (s41 "Sesto ordine cromatico ed enarmonico")
+    (s42 "Settimo ordine cromatico enarmonico")
+
+    (s43 "Del primo ordine cromatico ed enarmonico")
+    (s44 "Del secondo ordine cromatico")
+    (s45 "Del terzo ordine cromatico ed enarmonico")
+    (s46 "Del quarto ordine cromatico")
+    (s47 "Del quinto ordine cromatico ed enarmonico")
+    (s48 "Sesto ordine cromatico ed enarmonico")
+    (s49 "Settimo ordine cromatico enarmonico")
+
+    (s50 "Del primo ordine cromatico")
+    (s51 "Del secondo ordine cromatico")
+    (s52 "Del terzo ordine cromatico")
+    (s53 "Del quarto ordine cromatico")
+    (s54 "Del quinto ordine cromatico ed enarmonico")
+    (s55 "Sesto ordine cromatico ed enarmonico")
+    (s56 "Settimo ordine cromatico")
+
+    (s57 "Del primo ordine cromatico")
+    (s58 "Del secondo ordine cromatico")
+    (s59 "Del terzo ordine cromatico ed enarmonico")
+    (s60 "Del quarto ordine cromatico")
+    (s61 "Del quinto ordine cromatico ed enarmonico")
+    (s62 "Sesto ordine cromatico ed enarmonico")
+    (s63 "Settimo ordine cromatico enarmonico")
+    ))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   ;; Seite 1, fol. 136v, S. 271
+   (:text 110 58
+     (347 nil "Primo ordine Diatonico.")
+     (1411 nil "Secondo ordine ᴅiatonico."))
+   (:music 2352
+           (:section s1) gclef3 b38 sb0 b38 sb1 b38 sb2 b38 sb3 b38 sb4 b38 sb5 b38 sb6 b38 sb7 b38 sb8 b38 sb9 b38 sb10 b125
+           (:section s2) cclef1 b38 sb0 b22 b38 sb1 b22 b38 sb2 b38 b22 sb3 b38 sb4 b38 sb5 b38 sb6 b38 sb7 b38 sb8 b38 sb9 b38 sb10 b38 b38 bl)
+   (:text 110 58
+     (341 nil "Primo ordine ᴄro.")
+     (1409 nil "ᴅel secondo ordine cro."))
+   (:music 2343
+           (:section s8) gclef3 sh0 sb0 sh1 sb1 sh2 sb2 sh3 sb3 sh4 sb4 sh5 sb5 sh6 sb6 sg7 sb7 sh8 sb8 sh9 sb9 sh10 sb10 bl
+           (:section s9) gclef3 b22 sh0 sb0 b22 sb1 sh2 sb2 sh3 sb3 sb4 sb5 sh6 sb6 sh7 sb7 sb8 sh9 sb9 b22 sh10 sb10 b22 bl)
+   (:text 110 58
+     (343 nil "Del primo ordine Cro. & ᴇnar.")
+     (1404 nil "Del secondo ordine ᴄro."))
+   (:music 2350
+           (:section s15) cclef1 sh0 sb1 fl2 sbd2 sh2 sb2 sh3 sb3 fl5 sbd5 fl6 sbd6 sh6 sb6 sh7 sb7 fl9 sb9 sh9 sbd9 sh10 sb10 b22
+           (:section s16) cclef1 sh0 sb0 sh1 sb1 sh2 sb2 sh3 sb3 sh4 sb4 sh5 sb5 sh6 sb6 sh7 sb7 sh8 sb8 sh9 sb9 sh10 sb10)
+   (:text 110 58
+     (350 nil "Del primo ordine Cro.")
+     (1391 nil "ᴅel secondo ordine ᴄro."))
+   (:music 2332
+           (:section s22) cclef3 b22 sh0 sb1 sh1 sb2 sb2 sh3 sb3 sh4 sb4 sh5 sb5 sh6 sb6 b22 sh7 sb7 b22 sh8 sb8 sb9 b22 sh10 sb10 b38
+           (:section s23) cclef3 b22 sh0 sb0 b22 sb1 b22 sb2 sh3 b22 sb3 b22 sb4 b22 sb5 b22 sh6 b22 sb6 b22 sh7 sb7 b22 sb8 sb9 sh10 sb10 bl)
+   (:text 110 58
+     (345 nil "Del primo ordine Cro. & ᴇnar.")
+     (1388 nil "ᴅel secondo ordine Cro."))
+   (:music 2339
+           (:section s29) cclef5 sh0 sb0 fl2 sbd2 sh2 sb2 sh3 sbh sh4 sb4 fl6 sbd6 sh6 sb6 sh7 sb7 fl9 sbd9 sh9 sb9 sh10 sb10
+           (:section s30) cclef5 b22 sh0 sb0 sh1 sb1 sh2 sb2 sh3 sb3 sb4 sh5 sb5 sh6 sb6 sh7 sb7 sh8 sb8 sh9 sb9 sh10 sb10 bl)
+   (:text 110 58
+     (343 nil "Del primo ordine Crom.")
+     (1416 nil "ᴅel seoncdo ordine ᴄro."))
+   (:music 2323
+           (:section s36) cclef7 b22 sh0 sb0 b22 sh1 sb1 b22 sb2 b22 sh3 sb3 sh4 sb4 sh5 sb5 sb6 sh7 sb7 sh8 sb8 b22 sb9 b22 sh10 sb10 b38
+           (:section s37) cclef7 b22 sh0 sb0 b22 sb1 b22 sb2 b22 sh3 sb3 b22 sb4 b22 sb5 b22 sb6 b22 sh7 b22 sb7 b22 sb8 b22 sb9 b22 sh10 sb10 bl)
+   (:text 110 58
+     (339 nil "Del primo ordine ᴄro. & Enar.")
+     (1372 nil "Del seoncdo ordine Cro."))
+   (:music 2324
+           (:section s43) (:f-clef) max5 fclef5 sh0 sb0 sh1 sb1 sh2 sb2 sh3 sb3 sh4 sb4 fl6 sbd6 sh6 sb6 sh7 sb7 sh8 sb8 sh9 sb9 sh10 sb10 b38
+           (:section s44) (:f-clef))
+   ))
