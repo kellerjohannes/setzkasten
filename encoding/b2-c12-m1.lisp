240fccd526642e5b07e5def3a8898fa8d98f4437
Johannes Keller <johannkell@gmail.com>
2023-05-16 17:36:39 +0200

    working on b2-c12-m1

---
+++ encoding/b2-c12-m1.lisp
@@ -0,0 +1,26 @@
+`((:header
+   (:filename "b2-c12-m1")
+   (:alt-name "m2.015")
+   (:comment "q021_s065, rotation: 0, origin: 448")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "Esempio delle dissonanze sciolte, con li passaggi antichi,\\et moderni, a due voci")
+   (:section-headings
+    (s1 "All'antica")
+    (s2 "All'antica")
+    (s3 "Moderno")
+    (s4 "Non troppo moderne")
+    (s5 "Non moderne")))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 90 58
+     (177 1859 "Eſſempio delle diſſonanze ſciolte,con li paſſaggi antichi,et moderni. à due uoci."))
+   (:music nil
+           (:voice v1)
+           (:section s1)
+           )
+   (:text 70 58
+     (0 nil "")
+     )))
