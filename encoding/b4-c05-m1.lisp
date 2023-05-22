e168227162dfc373b8d2eba1476f49eb6e6794c3
Johannes Keller <johannkell@gmail.com>
2023-05-22 19:06:26 +0200

    working on b4-c07

---
+++ encoding/b4-c05-m1.lisp
@@ -0,0 +1,32 @@
+`((:header
+   (:filename "b4-c05-m1")
+   (:alt-name "m4.005")
+   (:comment "q021_s149, rotation: 0, origin: 486")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:line-headings
+    "Esempio della prolatione perfetta in tempo perfetto"
+    "Prolatione perfetta in tempo imperfetto"
+    "Esempio della prolatione imperfetta in tempo perfetto"
+    "Prolatione imperfetto in tempo imperfetto"))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 90 58
+          (0 1115 "Eſſempio della Prolatione perfet. in tēpo perfet.")
+          (1290 829 "Prolatione perfetta in tempo imper."))
+   (:music 2211
+           (:voice v1)
+           (:section s1) cclef7 b38 met-perf-maj b38 (:duration-override 3/2) sb8 b38 (:duration-override 1) m5 b38 m5 b38 m5 b38 b38 bl
+           (:section s2) b38 b38 (:duration-override 3/2) br8 b38 (:duration-override 1) sb5 b38 sb5 b38 sb5 b38 b38 b125
+           (:section s3) (:newline) cclef7 b38 met-imperf-maj b38 (:duration-override 3/2) sb8 b38 (:duration-override 1) m6 b38 m6 b38 m6 b38 b38 bl
+           (:section s4) b38 b38 br4 b38 sb6 b38 sb6 b38 bl b22 b22)
+   (:text 90 58
+          (174 1011 "Eſſempio della Prolat.imper.in tēpo ꝑfetto")
+          (1268 857 "Prolatione imperfet. in tempo imper."))
+   (:music 2218
+           (:section s5) (:newline) cclef7 b38 met-perf-min b22 b22 sb7 b38 b22 m5 b38 b22 m5 b38 b38 bl
+           (:section s6) b38 b38 br6 b38 sb8 b38 b22 sb8 b38 b22 sb8 b38 b38 b125
+           (:section s7) (:newline) cclef7 b38 met-imperf-min b38 b22 sb8 b38 b22 m5 b38 b22 m5 b38 b38 bl
+           (:section s8) b38 b38 br4 b38 sb6 b38 sb6 b38 bl b22 b38)))
---
+++ encoding/b4-c07-m1.lisp
@@ -0,0 +1,29 @@
+`((:header
+   (:filename "b4-c07-m1")
+   (:alt-name "m4.006")
+   (:comment "q021_s151, rotation: , origin: 425")
+   (:creator "Vicentino21 / Johannes Keller")
+   (:title "Esempio delle pause notate nel tempo perfetto,\\& nel imperfetto")
+   (:section-captions
+    ;; Setzung der captions gemäss Bibelgruppe
+    (s1 "[Buono]")
+    (s2 "[Buono]")
+    (s3 "Non buono")
+    (s4 "[Non buono]")
+    (s5 "Non buono")
+    (s6 "[Non buono]")))
+  (:preamble-type-imitation
+   (:width nil)
+   (:height nil)
+   (:background ,*score-bg-color*))
+  (:data
+   (:text 90 58
+          (351 1548 "")
+          )
+   (:music nil
+           (:voice v1)
+           (:section s1)
+           )
+   (:text 70 58
+          (0 nil "")
+          )))
