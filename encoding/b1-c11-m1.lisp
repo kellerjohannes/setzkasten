a371eed78c519d3ef6ce9e9ae9147b6794f53824
Anne Smith <anne.smith@fhnw.ch>
2023-09-30 15:40:35 +0200

    Update b1-c11-m1.lisp

--- encoding/b1-c11-m1.lisp
+++ encoding/b1-c11-m1.lisp
@@ -12,6 +12,7 @@
               (:en "tono divided in 4\\ascending parts"))))
    (:section-captions
     (s1 (:alt (:it "per ♮: ut-re-mi-fa-re")
+              ;; Kommentar Anne: bessere Unterlegung der Silben unter den Noten in allen Sprachen?
               (:de "")
               (:en "per ♮: ut-re-mi-fa-re")))
     (s2 (:alt (:it "per ♭: re-mi-fa-re-mi")
