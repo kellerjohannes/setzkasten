0779efad917df381ef1077bd70a029ac0da7e4dc
Anne Smith <anne.smith@fhnw.ch>
2023-11-21 11:49:23 +0100

    English translation

--- encoding/b3-c21-m2.lisp
+++ encoding/b3-c21-m2.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Esempio del settimo modo per ♮ incitato\\della musica participata et mista")
                  (:de "")
-                 (:en ""))))
+                 (:en "Example of the seventh mode in ♮ incitato\\of musica participata and mista"))))
   (:preamble-lilypond
    (:clef-overrides
     (s1 v1 "treble_8")))
