bd15db69817fcbb703ea1f191217f9876b1a4a29
Anne Smith <anne.smith@fhnw.ch>
2023-12-07 14:11:02 +0100

    Update b3-c36-m1.lisp, Kommentar Anne

--- encoding/b3-c36-m1.lisp
+++ encoding/b3-c36-m1.lisp
@@ -10,6 +10,7 @@
     (s2 (:alt (:it "Prima quarta\\cromatica")
               (:de "")
               (:en "First quarta\\chromatic")))))
+              ;;Kommentar Anne: Ich habe etwas Mühe hier und in Kapitel 36-37, dass die Texte nur auf einer Seite des Beispiels sind, insbesondere im englischen, weil das Adjektiv eigentlich vor dem Substantiv stehen sollten. Wenn sie auf entgegengesetzte Seiten der Musik stehen, deuten sie zwei verschiedenen Ebenen an. Gilt auch für Kapitel 37, 45-46.
   (:data
    (:text 70 58
      (49 nil "Prima Quarta.")
