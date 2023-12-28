3388bd8daf81e04e4ba149bcced6815e06080535
Martin Kirnbauer <martin.kirnbauer@fhnw.ch>
2023-12-28 11:15:55 +0100

    Update b1-c05-m2.lisp

--- encoding/b1-c05-m2.lisp
+++ encoding/b1-c05-m2.lisp
@@ -4,7 +4,7 @@
    (:comment "q021_s022, origin: 1023")
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Dimostratione della mano cromatica, ascendente, con li semitoni minore,\\e con l'essempio")
-                 (:de "")
+                 (:de "Darstellung der chromatischen Hand, aufsteigend, mit den semitoni minori, und mit dem Beispiel.")
                  (:en "Illustration of the ascending chromatic hand, with the semitoni minori, and with an example")))
    (:section-headings
     (s1 (:alt (:it "Alamire")
