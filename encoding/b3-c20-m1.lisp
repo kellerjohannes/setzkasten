28065b6311d34d681fbbba9d40b3a915873045d7
Martin Kirnbauer <martin.kirnbauer@fhnw.ch>
2024-01-27 15:12:18 +0100

    Update b3-c20-m1.lisp

--- encoding/b3-c20-m1.lisp
+++ encoding/b3-c20-m1.lisp
@@ -4,7 +4,7 @@
    (:comment "q021_s099, origin: 414")
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Esempio del sesto modo per ♮ incitato,\\della musica participata e mista")
-                 (:de "")
+                 (:de "Beispiel des sechsten Modus per ♮ incitato\\der musica participata und mista.")
                  (:en "Example of the sixth mode in ♮ incitato,\\of musica participata and mista"))))
   (:data
    (:text 110 58
