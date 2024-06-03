8ada342e56b1e0ed7389d7da496c58add5034f31
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-03 22:18:14 +0200

    Update b2-c23-m1.lisp

--- encoding/b2-c23-m1.lisp
+++ encoding/b2-c23-m1.lisp
@@ -3,14 +3,14 @@
    (:alt-name "m2.026")
    (:comment "q021_s074, rotation: -0.49, origin: 1000")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Esempi d'alcune ottave doppie")
+   (:title (:alt (:it "Esempi d'alcune Ottaue doppie.")
                  (:de "Beispiele von einigen ottave doppie")
                  (:en "Examples of some ottave doppie")))
    (:line-headings
-    (:alt (:it "Naturali")
+    (:alt (:it "naturali")
           (:de "naturali")
           (:en "Naturali"))
-    (:alt (:it "Accidentali")
+    (:alt (:it "accidentali")
           (:de "accidentali")
           (:en "Accidentali")))
    (:section-captions
