d270eed77d633f360828761e6304442b91df416a
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-04 16:39:27 +0200

    Update b3-c32-m8.lisp

--- encoding/b3-c32-m8.lisp
+++ encoding/b3-c32-m8.lisp
@@ -3,9 +3,11 @@
    (:alt-name "m3.070")
    (:comment "q021_s113, rotation: 1.31, origin: 513")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Essempio di molte Cadentie dell'ottavo modo [à quattro uoci] della musica participata e mista")
+   (:title (:alt (:it "Essempio di molte Cadentie dell'ottavo modo [à quattro uoci] della musica participata e mista [per ♮ quadro].")
                  (:de "Beispiel von vielen Kadenzen des achten Modus der musica participata und mista")
                  (:en "Example of many cadences of the eighth mode, of musica participata and mista")))
+                 ;; LUIGI: Ich würde [à quattro uoci] und [per ♮ quadro] einfügen.
+                 ;;OPEN
    (:voice-labels
     (s1 v1 (:alt (:it "Cadentie del Soprano")
                  (:de "Kadenzen des Soprano")
