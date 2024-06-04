f6f348e1b04f55f0d28ce8115246723d58f360e1
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-04 10:59:54 +0200

    Update b3-c17-m4.lisp

--- encoding/b3-c17-m4.lisp
+++ encoding/b3-c17-m4.lisp
@@ -3,8 +3,8 @@
    (:alt-name "m3.029")
    (:comment "q021_s097, rotation: 0.96, origin: 546")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Esempio del terzo modo per musica finta, participata e mista")
-                 (:de "Beispiel des dritten Modus per musica finta [der musica] participata und mista.")
+   (:title (:alt (:it "Essempio del terzo Modo per musica finta, [della Musica] participata & mista.")
+                 (:de "Beispiel des dritten Modus per musica finta [der musica] participata und mista")
                  (:en "Example of the third mode in musica finta, participata and mista"))))
   (:data
    (:text 110 58
