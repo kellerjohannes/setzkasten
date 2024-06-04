9c34bf338c51fb0b0247aea2751d2b144f588b02
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-04 10:50:29 +0200

    Update b3-c16-m4.lisp

--- encoding/b3-c16-m4.lisp
+++ encoding/b3-c16-m4.lisp
@@ -3,9 +3,11 @@
    (:alt-name "m3.025")
    (:comment "q021_s097, rotation: 0.96, origin: 544")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Esempio del secondo modo per musica finta,\\et è simile alla participata et mista")
+   (:title (:alt (:it "Essempio del secondo Modo per musica finta,\\& è simile alla [Musica] participata & mista.")
                  (:de "Beispiel des zweiten Modus per musica finta,\\und es ist ähnlich zur [musica] participata und mista.")
                  (:en "Example of the second mode in musica finta,\\ and it is similar to the participata and mista"))))
+                 ;; LUIGI for ANN: 'Musica' to add also in the title in english?
+                 ;; OPEN
   (:data
    (:text 90 58
           (151 1939 "Eſſempio del ſecondo Modo per muſica finta, & è ſimile alla participata & miſta."))
