8d797d4c8cdf40fcba54f9566009ac6a7255f490
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-04 11:31:33 +0200

    Update b3-c20-m4.lisp

--- encoding/b3-c20-m4.lisp
+++ encoding/b3-c20-m4.lisp
@@ -3,8 +3,8 @@
    (:alt-name "m3.042")
    (:comment "q021_s099, origin: 429")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Esempio del sesto modo, detto per musica finta,\\della musica participata e mista")
-                 (:de "Beispiel des sechsten Modus, genannt per musica finta,\\der musica participata und mista.")
+   (:title (:alt (:it "Essempio del sesto Modo, detto per musica finta,\\della Musica participata & mista.")
+                 (:de "Beispiel des sechsten Modus, genannt per musica finta,\\der musica participata und mista")
                  (:en "Example of the sixth mode, called music finta,\\of musica participata and mista"))))
   (:data
    (:text 110 58
