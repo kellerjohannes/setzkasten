59fa421bf9e7d9c1fca25700c80dbccac9836e4e
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-04 10:57:54 +0200

    Update b3-c17-m3.lisp

--- encoding/b3-c17-m3.lisp
+++ encoding/b3-c17-m3.lisp
@@ -3,8 +3,8 @@
    (:alt-name "m3.028")
    (:comment "q021_s097, rotation: 0.96, origin: 552")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Esempio del terzo modo per ♭ molle,\\della musica participata e mista")
-                 (:de "Beispiel des dritten Modus per ♭ molle\\der musica participata und mista.")
+   (:title (:alt (:it "Essempio del terzo Modo per ♭ molle,\\della Musica participata & mista.")
+                 (:de "Beispiel des dritten Modus per ♭ molle\\der musica participata und mista")
                  (:en "Example of the third mode in ♭ molle,\\of musica participata and mista"))))
   (:data
    (:text 110 58
