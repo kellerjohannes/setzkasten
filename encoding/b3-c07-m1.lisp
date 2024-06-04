349563bec262ef45e136ae800190a967b4aacf45
Luigi Collarile <luigi.collarile@fhnw.ch>
2024-06-04 09:51:15 +0200

    Update b3-c07-m1.lisp

--- encoding/b3-c07-m1.lisp
+++ encoding/b3-c07-m1.lisp
@@ -3,8 +3,8 @@
    (:alt-name "m3.006")
    (:comment "q021_s89, rotation: 0.89, origin: 476")
    (:creator "Vicentino21 / Johannes Keller")
-   (:title (:alt (:it "Esempio del terzo modo diatonico semplice,\\da greci detto frigio")
-                 (:de "Beispiel des einfachen dritten diatonischen Modus,\\von den Griechen Phrygisch genannt.")
+   (:title (:alt (:it "Essempio del terzo modo Diatonico semplice,\\da Greci detto Frigio.")
+                 (:de "Beispiel des einfachen dritten diatonischen Modus,\\von den Griechen Phrygisch genannt")
                  (:en "Example of the third simple diatonic mode,\\called Phrygian by the Greeks"))))
   (:data
    (:text 90 58
