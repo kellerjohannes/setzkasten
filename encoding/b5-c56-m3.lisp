b3bd2c5765f5bb4c5585f60c49e1631d7809e3bc
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 18:16:40 +0200

    Update b5-c56-m3.lisp

--- encoding/b5-c56-m3.lisp
+++ encoding/b5-c56-m3.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Regola di ritrovare le sette ottave sopra Dsolre terzo,\\con li sottoscritti esempi")
                  (:de "Methode, die sieben ottave über Dsolre terzo zu finden,\\mit den unten geschriebenen Beispielen.")
-                 (:en "Method for finding the seven ottave on Dsolre terzo,\\with the examples written below")))
+                 (:en "Rule for finding the seven ottave on Dsolre terzo,\\with the examples written below")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
