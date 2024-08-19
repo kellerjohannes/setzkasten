52a9b8d2a3dc57d242f8a8f8e7fb1c88c1749ad0
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 18:04:41 +0200

    Update b5-c54-m3.lisp

--- encoding/b5-c54-m3.lisp
+++ encoding/b5-c54-m3.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Regola di ritrovare le sette ottave sopra Cfaut quarto,\\con i sottoscritti esempi")
                  (:de "Methode, die sieben ottave über Cfaut quarto zu finden,\\mit den unten geschriebenen Beispielen.")
-                 (:en "Method for finding the seven ottave on Cfaut quarto,\\with the examples written below")))
+                 (:en "Rule for finding the seven ottave on Cfaut quarto,\\with the examples written below")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
