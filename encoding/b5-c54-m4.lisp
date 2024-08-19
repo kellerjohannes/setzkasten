bc815fa22619bf747313f81c628ee387ff6efb09
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 18:05:20 +0200

    Update b5-c54-m4.lisp

--- encoding/b5-c54-m4.lisp
+++ encoding/b5-c54-m4.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Regola di ritrovare le sette ottave sopra Dsolre quarto,\\con i sottoscritti esempi")
                  (:de "Methode, die sieben ottave über Dsolre quarto zu finden,\\mit den unten geschriebenen Beispielen.")
-                 (:en "Method for finding the seven ottave on Dsolre quarto,\\with the examples written below")))
+                 (:en "Rule for finding the seven ottave on Dsolre quarto,\\with the examples written below")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
