dd8a1a6ac45fba158b32157a224e644b039112ff
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 18:07:43 +0200

    Update b5-c54-m7.lisp

--- encoding/b5-c54-m7.lisp
+++ encoding/b5-c54-m7.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Regola di ritrovare le sette ottave sopra Gsolreut [quarto],\\con i sottoscritti esempi")
                  (:de "Methode, die sieben ottave über Gsolreut quarto zu finden,\\mit den unten geschriebenen Beispielen.")
-                 (:en "Method of finding the seven ottave on Gsolreut [quarto],\\with the examples written below")))
+                 (:en "Rule of finding the seven ottave on Gsolreut [quarto],\\with the examples written below")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
