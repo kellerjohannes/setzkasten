f8c4b071ca4b6a3620c096433f3c2ed854c0d29b
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 17:58:56 +0200

    Update b5-c53-m6.lisp

--- encoding/b5-c53-m6.lisp
+++ encoding/b5-c53-m6.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Regola di ritrovare le sette ottave sopra Ffaut primo,\\con i sottoscritti esempi")
                  (:de "Methode, die sieben ottave über Ffaut primo zu finden,\\mit den unten geschriebenen Beispielen.")
-                 (:en "Method of finding the seven ottave on Ffaut primo,\\with the examples written below")))
+                 (:en Rule of finding the seven ottave on Ffaut primo,\\with the examples written below")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
