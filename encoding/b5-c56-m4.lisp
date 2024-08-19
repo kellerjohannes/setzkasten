a9f69a524d4104e15fdcc002a91a9633d3575c89
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 18:17:20 +0200

    Update b5-c56-m4.lisp

--- encoding/b5-c56-m4.lisp
+++ encoding/b5-c56-m4.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "Regola di ritrovare le sette ottave sopra Elami secondo,\\con li sottoscritti esempi")
                  (:de "Methode, die sieben ottave über Elami terzo zu finden,\\mit den unten geschriebenen Beispielen.")
-                 (:en "Method for finding the seven ottave on Elami secondo,\\with the examples written below")))
+                 (:en "Rule for finding the seven ottave on Elami secondo,\\with the examples written below")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
