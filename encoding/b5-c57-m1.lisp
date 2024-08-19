28935a900416da2574c4bd18e85f7b8cde36e171
Anne Smith <anne.smith@fhnw.ch>
2024-08-19 18:18:52 +0200

    Update b5-c57-m1.lisp

--- encoding/b5-c57-m1.lisp
+++ encoding/b5-c57-m1.lisp
@@ -5,7 +5,7 @@
    (:creator "Vicentino21 / Johannes Keller")
    (:title (:alt (:it "[Regola di ritrovare le sette ottave sopra Are quinto,\\con li sottoscritti esempi]")
                  (:de "[Methode, die sieben ottave über Are quinto zu finden,\\mit den unten geschriebenen Beispielen.]")
-                 (:en "[Method for finding the seven ottave on Are quinto,\\with the examples written below]")))
+                 (:en "[Rule for finding the seven ottave on Are quinto,\\with the examples written below]")))
    (:section-headings
     (s1 (:alt (:it "Prima ottava")
               (:de "erste ottava")
